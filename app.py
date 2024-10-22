e7e33af1fdb6ff8a742b6c6dd24c1f35439d95b5
Guilherme Gomes <134429767+GuiGomesD@users.noreply.github.com>
2024-10-22 15:10:55 -0300

    render

--- app.py
+++ app.py
@@ -2,6 +2,7 @@ from flask import Flask, render_template
 import sqlite3
 from datetime import datetime
 from dateutil import parser
+import os
 
 app = Flask(__name__)
 
@@ -16,9 +17,9 @@ def get_matches_from_db():
     for match in matches:
         home_team, away_team, score, stadium, date_str, referee, yellow_cards, red_cards = match
         
-        #Usando dateutil.parser para analisar a data
-        date = parser.isoparse(date_str)  #Analisa a string no formato ISO 8601
-        formatted_date = date.strftime('%d/%m/%Y %H:%M')  #Exemplo: 21/10/2022 15:00
+        # Usando dateutil.parser para analisar a data
+        date = parser.isoparse(date_str)  # Analisa a string no formato ISO 8601
+        formatted_date = date.strftime('%d/%m/%Y %H:%M')  # Exemplo: 21/10/2022 15:00
         formatted_matches.append((home_team, away_team, score, stadium, formatted_date, referee, yellow_cards, red_cards))
     
     return formatted_matches
@@ -32,4 +33,5 @@ def index():
     return render_template('index.html', matches=matches)
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    port = int(os.environ.get('PORT', 5000))  # Obtém a porta do ambiente ou usa 5000 como padrão
+    app.run(host='0.0.0.0', port=port, debug=True)
